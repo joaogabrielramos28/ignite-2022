7c034c93640bfc92ab1547a62bcf788792dfd733
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2022-09-20 23:28:34 -0300

    ✨ feat: Header component

--- React/projeto-01/src/App.tsx
+++ React/projeto-01/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import { Header } from "./components/Header";
-import "./styles/global.css";
 
 function App() {
   return <Header />;
--- React/projeto-01/src/components/Header/index.tsx
+++ React/projeto-01/src/components/Header/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Logo from "../../assets/Logo.png";
+import styles from "./styles.module.css";
 
 export const Header: React.FC = () => {
-  return <header className="header"></header>;
+  return (
+    <header className={styles.header}>
+      <img src={Logo} alt={"todo logo"} />
+    </header>
+  );
 };
