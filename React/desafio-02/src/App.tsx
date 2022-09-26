56af3ba99e209f1d54409795435d8e59f29bbb50
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2022-09-26 20:45:04 -0300

    💄 feat: Setup theme with styled components

--- React/desafio-02/src/App.tsx
+++ React/desafio-02/src/App.tsx
@@ -1,5 +1,8 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./theme/theme";
+
 function App() {
-  return <></>;
+  return <ThemeProvider theme={defaultTheme}></ThemeProvider>;
 }
 
 export default App;
