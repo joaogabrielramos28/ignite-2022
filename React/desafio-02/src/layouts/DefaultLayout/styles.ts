31604e00c912e8a677199433acdd2f7dfd749ec8
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2022-09-27 00:27:18 -0300

    🐛 fix: Layout bug

--- React/desafio-02/src/layouts/DefaultLayout/styles.ts
+++ React/desafio-02/src/layouts/DefaultLayout/styles.ts
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
   width: 100%;
+  max-width: 76rem;
   height: calc(100vh - 10rem);
-  padding: 0 10rem;
+  margin: 0 auto;
+
   background: ${({ theme }) => theme.color.background};
-  display: flex;
-  flex-direction: column;
 `;
--- React/desafio-02/src/pages/Home/components/Intro/styles.ts
+++ React/desafio-02/src/pages/Home/components/Intro/styles.ts
@@ -1,24 +1,12 @@
 import styled, { css } from "styled-components";
-import { rgba } from "polished";
-import bg from "../../../../assets/intro-background.png";
 
 export const IntroContainer = styled.section`
   width: 100%;
   height: 34rem;
 
-  background: ${({ theme }) => `url(${bg}) no-repeat center,
-      linear-gradient(
-        0deg,
-        ${theme.color.white} 0%,
-        ${rgba(theme.color.background, 0.2)} 50%,
-        ${theme.color.background} 100%
-      )`};
-
-  background-size: cover;
-  padding: 1rem 0rem;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 
   img {
     margin-left: 3.5rem;
