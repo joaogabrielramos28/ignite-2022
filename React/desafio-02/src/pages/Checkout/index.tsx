32c00fce22353adf26b77032a3f265f249a84d00
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2022-09-27 23:59:23 -0300

    🚧 feat: Starting checkout page

---
+++ React/desafio-02/src/pages/Checkout/index.tsx
@@ -0,0 +1,3 @@
+export const Checkout: React.FC = () => {
+  return <></>;
+};
--- React/desafio-02/src/routes/index.routes.tsx
+++ React/desafio-02/src/routes/index.routes.tsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { DefaultLayout } from "../layouts/DefaultLayout";
+import { Checkout } from "../pages/Checkout";
 import { Home } from "../pages/Home";
 export const Router: React.FC = () => {
   return (
@@ -7,6 +8,7 @@ export const Router: React.FC = () => {
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Route>
       </Routes>
     </BrowserRouter>
