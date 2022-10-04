4f2c3b66593f8396fc9293dd682e6cac1081d86a
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2022-10-03 22:15:46 -0300

    🎨 fix: checkout page structure

--- React/desafio-02/src/pages/Checkout/index.tsx
+++ React/desafio-02/src/pages/Checkout/index.tsx
@@ -1,5 +1,5 @@
-import { Form } from "./Form";
-import { OrderDetails } from "./Form/components/OrderDetails";
+import { Form } from "./components/Form";
+import { OrderDetails } from "./components/OrderDetails";
 import { CheckoutContainer } from "./styles";
 
 export const Checkout: React.FC = () => {
