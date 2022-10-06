aeac7f976cc22a1c64b225eeaee3697e37331971
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2022-10-06 19:36:10 -0300

    💄 feat: Configuring theme

--- React/dt-money/src/App.tsx
+++ React/dt-money/src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
-  return <h2>Dt Money</h2>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h2>ola</h2>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
