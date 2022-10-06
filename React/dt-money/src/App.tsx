798e6101a1b003f3cb3701e2d34a2d6d45673057
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2022-10-06 20:09:08 -0300

    ✨ feat: Creating logo

--- React/dt-money/src/App.tsx
+++ React/dt-money/src/App.tsx
@@ -1,12 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h2>ola</h2>
-
+      <Transactions />
       <GlobalStyle />
     </ThemeProvider>
   );
