3019d482908937a62433f149c092b3ccc370585c
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-01-18 22:36:52 -0300

    Starting challenge01 from react native

---
+++ React-Native/desafio01/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Desafio01</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
