bdf69f5087ed40bb2d2e07492b8e62dad1461490
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-01-26 16:38:45 -0300

    feat: button component

--- React-Native/desafio02/src/screens/Groups/index.tsx
+++ React-Native/desafio02/src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Highlight } from "@components/Highlight";
 import { FlatList } from "react-native";
 import { Container } from "./styles";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export const Groups = () => {
   const [groups, setGroups] = useState<string[]>([]);
@@ -22,6 +23,7 @@ export const Groups = () => {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         }
       />
+      <Button title="Cadastrar turma" />
     </Container>
   );
 };
