8a795e6ca516525b4b811b55347d3bb63cc4f8b7
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-01-26 16:34:16 -0300

    feat: ListEmpty component

--- React-Native/desafio02/src/screens/Groups/index.tsx
+++ React-Native/desafio02/src/screens/Groups/index.tsx
@@ -1,16 +1,27 @@
+import React, { useState } from "react";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import React from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
 
 export const Groups = () => {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        }
+      />
     </Container>
   );
 };
