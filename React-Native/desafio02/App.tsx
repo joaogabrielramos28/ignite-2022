ff5844a4a48ddb18169d6b4415d7d5e7888ce15f
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-01-26 15:55:52 -0300

    feat: creating header and add status bar

--- React-Native/desafio02/App.tsx
+++ React-Native/desafio02/App.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "styled-components";
 import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { useFonts } from "expo-font";
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,6 +14,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={"light-content"}
+        translucent
+        backgroundColor={"transparent"}
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
--- React-Native/desafio02/src/screens/Groups/index.tsx
+++ React-Native/desafio02/src/screens/Groups/index.tsx
@@ -1,6 +1,11 @@
+import { Header } from "@components/Header";
 import React from "react";
 import { Container } from "./styles";
 
 export const Groups = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 };
