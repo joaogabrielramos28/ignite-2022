3dd01ca917bb69dfc355d13460dc663018e4f7a7
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-02-18 17:07:01 -0300

    feat: using async storage for store groups

--- React-Native/desafio02/src/screens/Groups/index.tsx
+++ React-Native/desafio02/src/screens/Groups/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -6,7 +6,8 @@ import { FlatList } from "react-native";
 import { Container } from "./styles";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { groupsGetAll } from "@storage/group/groupGetAll";
 
 export const Groups = () => {
   const [groups, setGroups] = useState<string[]>([]);
@@ -15,6 +16,25 @@ export const Groups = () => {
   const handleNewGroup = () => {
     navigate("new");
   };
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleOpenGroup(group: string) {
+    navigate("players", { group });
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
   return (
     <Container>
       <Header />
@@ -23,7 +43,9 @@ export const Groups = () => {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
--- React-Native/desafio02/src/screens/NewGroup/index.tsx
+++ React-Native/desafio02/src/screens/NewGroup/index.tsx
@@ -6,13 +6,30 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
+import { groupCreate } from "@storage/group/groupCreate";
+import { Alert } from "react-native";
+import { AppError } from "@utils/AppError";
 
 export function NewGroup() {
   const navigation = useNavigation();
   const [group, setGroup] = useState("");
 
-  function handleNew() {
-    navigation.navigate("players", { group });
+  async function handleNew() {
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma.");
+      }
+
+      await groupCreate(group);
+      navigation.navigate("players", { group });
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+        console.log(error);
+      }
+    }
   }
   return (
     <Container>
