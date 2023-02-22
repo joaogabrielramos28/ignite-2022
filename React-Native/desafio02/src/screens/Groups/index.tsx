8e521dae3413eb513bba0bb33cebf113d3dba6ec
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-02-22 18:36:00 -0300

    feat: finish project

--- React-Native/desafio02/src/screens/Groups/index.tsx
+++ React-Native/desafio02/src/screens/Groups/index.tsx
@@ -8,10 +8,12 @@ import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { groupsGetAll } from "@storage/group/groupGetAll";
+import { Loading } from "@components/Loading";
 
 export const Groups = () => {
   const [groups, setGroups] = useState<string[]>([]);
   const { navigate } = useNavigation();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleNewGroup = () => {
     navigate("new");
@@ -19,10 +21,13 @@ export const Groups = () => {
 
   async function fetchGroups() {
     try {
+      setIsLoading(true);
       const data = await groupsGetAll();
       setGroups(data);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -39,18 +44,22 @@ export const Groups = () => {
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={groups}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+          )}
+          contentContainerStyle={groups.length === 0 && { flex: 1 }}
+          ListEmptyComponent={
+            <ListEmpty message="Que tal cadastrar a primeira turma?" />
+          }
+        />
+      )}
 
-      <FlatList
-        data={groups}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => (
-          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
-        )}
-        contentContainerStyle={groups.length === 0 && { flex: 1 }}
-        ListEmptyComponent={
-          <ListEmpty message="Que tal cadastrar a primeira turma?" />
-        }
-      />
       <Button title="Cadastrar turma" onPress={handleNewGroup} />
     </Container>
   );
