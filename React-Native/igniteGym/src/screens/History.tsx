b57a9948e7ba02aa9f246eaa1f834373e261c100
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-03-15 00:03:07 -0300

    ✨ feat: starting history structure

--- React-Native/igniteGym/src/screens/History.tsx
+++ React-Native/igniteGym/src/screens/History.tsx
@@ -1,9 +1,43 @@
-import { Center, Text } from "native-base";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Heading, SectionList, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export const History = () => {
+  const [exercises, setExercises] = useState([
+    {
+      title: "26.02.23",
+      data: ["Puxada", "Agachamento", "Abdominal"],
+    },
+    {
+      title: "28.02.23",
+      data: ["Puxada", "Agachamento"],
+    },
+  ]);
   return (
-    <Center flex={1}>
-      <Text>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {title}
+          </Heading>
+        )}
+        contentContainerStyle={
+          [].length === 0 && { flex: 1, justifyContent: "center" }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercicios registrados ainda.{"\n"} Vamo fazer exercícios
+            hoje?
+          </Text>
+        )}
+        px={8}
+      />
+    </VStack>
   );
 };
