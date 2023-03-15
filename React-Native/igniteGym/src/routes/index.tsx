9a3850ecd3a12970fd0be8bf1431418bc363c256
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-03-14 23:02:16 -0300

    feat: :sparkles: private routes

--- React-Native/igniteGym/src/routes/index.tsx
+++ React-Native/igniteGym/src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export const Routes = () => {
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      <AppRoutes />
     </NavigationContainer>
   );
 };
