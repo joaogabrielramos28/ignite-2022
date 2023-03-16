99bdc4d433db8ef2c84c1bf30ac6d9bf1d7c2d25
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-03-15 23:42:20 -0300

    ✨ feat: add image picker for have access to camera library and change user profile photo

--- React-Native/igniteGym/src/screens/Profile.tsx
+++ React-Native/igniteGym/src/screens/Profile.tsx
@@ -9,14 +9,60 @@ import {
   ScrollView,
   Skeleton,
   Text,
+  useToast,
   VStack,
 } from "native-base";
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 export const Profile = () => {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState<string>(
+    "https://avatars.githubusercontent.com/u/5466341?v=4"
+  );
   const PHOTO_SIZE = 33;
+
+  const toast = useToast();
+
+  const handleUserPhotoSelect = async () => {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          toast.show({
+            title: "A imagem deve ser menor que 5MB",
+            placement: "top",
+            bgColor: "red.500",
+          });
+          return;
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  };
+
   return (
     <KeyboardAvoidingView behavior="padding" flex={1}>
       <VStack flex={1}>
@@ -30,7 +76,7 @@ export const Profile = () => {
             {!photoIsLoading ? (
               <UserPhoto
                 source={{
-                  uri: "https://avatars.githubusercontent.com/u/5466341?v=4",
+                  uri: userPhoto,
                 }}
                 alt="Foto do usuário"
                 size={PHOTO_SIZE}
@@ -44,7 +90,7 @@ export const Profile = () => {
                 endColor="gray.400"
               />
             )}
-            <TouchableOpacity>
+            <TouchableOpacity onPress={handleUserPhotoSelect}>
               <Text
                 color="green.500"
                 fontWeight="bold"
