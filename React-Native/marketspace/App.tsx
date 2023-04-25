2d2673e9e126c77d682107a969e6870f51db68e8
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-04-25 17:41:32 -0300

    chore: installing and configure nativebase

--- React-Native/marketspace/App.tsx
+++ React-Native/marketspace/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
-    <View>
+    <NativeBaseProvider>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </NativeBaseProvider>
   );
 }
