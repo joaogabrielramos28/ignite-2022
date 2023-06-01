ea89b53f20c48a31868269aedf019455b9e3dcf1
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-06-01 00:34:26 -0300

    feat: configuring oauth2 google

--- React-Native/ignitefleet/src/screens/SignIn/index.tsx
+++ React-Native/ignitefleet/src/screens/SignIn/index.tsx
@@ -1,16 +1,57 @@
+import { Alert } from "react-native";
 import { Container, Title, Slogan } from "./styles";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
 
 import backgroundImg from "../../assets/background.png";
 import { Button } from "../../components/Button";
+import { IOS_CLIENT_ID } from "@env";
+import { useEffect, useState } from "react";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [_, response, googleSignIng] = Google.useAuthRequest({
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ["profile", "email"],
+  });
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true);
+
+    googleSignIng().then((response) => {
+      if (response?.type !== "success") {
+        setIsAuthenticating(false);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      if (response.authentication?.idToken) {
+        setIsAuthenticating(false);
+      } else {
+        Alert.alert(
+          "Entrar",
+          "Não foi possível conectar-se a sua conta google."
+        );
+        setIsAuthenticating(false);
+      }
+    }
+  }, [response]);
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
 
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title="Entrar com Google" />
+      <Button
+        title="Entrar com Google"
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
     </Container>
   );
 }
