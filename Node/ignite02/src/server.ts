fec6b5ee88a8841341a8446f86ee6ee73f9024dd
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-09-28 22:34:08 -0300

    feat: finish module 2

--- Node/ignite02/src/server.ts
+++ Node/ignite02/src/server.ts
@@ -1,17 +1,25 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { randomUUID } from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/', async (request, reply) => {
-  const tables = await knex('sqlite_schema').select('*')
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running on port 3333 🎈')
