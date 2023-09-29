5cd5108f3fca37c50fd9409a27b41cd6bf28369a
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2023-09-29 18:50:46 -0300

    feat: finish last module

--- Node/ignite02/src/env/index.ts
+++ Node/ignite02/src/env/index.ts
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
+  DATABASE_CLIENT: z.enum(['pg', 'sqlite']).default('pg'),
   PORT: z.coerce.number().default(3333),
 })
 
