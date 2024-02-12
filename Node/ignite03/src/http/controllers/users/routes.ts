95f9f1e7432e164a9b41baff54d1f0c3e93222be
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2024-02-12 14:22:23 -0300

    feat: role by user and refresh token

--- Node/ignite03/src/http/controllers/users/routes.ts
+++ Node/ignite03/src/http/controllers/users/routes.ts
@@ -3,11 +3,13 @@ import { register } from './register.controller'
 import { authenticate } from './authenticate.controller'
 import { profile } from './profile.controller'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { refreshToken } from './refresh-token.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  app.post('/token/refresh', refreshToken)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
--- Node/ignite03/src/http/middlewares/verify-jwt.ts
+++ Node/ignite03/src/http/middlewares/verify-jwt.ts
@@ -2,6 +2,11 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function verifyJWT(request: FastifyRequest, reply: FastifyReply) {
   try {
+    const token = request.headers.authorization
+
+    if (!token) {
+      throw new Error('Token not found')
+    }
     await request.jwtVerify()
   } catch (err) {
     reply.status(401).send({ message: 'Unauthorized' })
